2a870d8495a05c85fa38c01fc38f3322fccfb85b
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-09-20 13:14:52 +0300

    добавил стартовый шаблон и функционал

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './sass/main.scss';
+
+const timer = {
+    start() {
+        const startTime = Date.now();
+
+        startTime -
+            setInterval(() => {
+                const currentTime = Date.now();
+                const deltaTime = currentTime - startTime;
+                const time = getTimeComponents(deltaTime);
+                clockInterface(time);
+            }, 1000);
+    },
+};
+timer.start(400000);
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    const hours = pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+}
+
+function clockInterface({ days, hours, mins, secs }) {
+    document.querySelector('[data-value="days"]').textContent = days;
+    document.querySelector('[data-value="hours"]').textContent = hours;
+    document.querySelector('[data-value="mins"]').textContent = mins;
+    document.querySelector('[data-value="secs"]').textContent = secs;
+}
