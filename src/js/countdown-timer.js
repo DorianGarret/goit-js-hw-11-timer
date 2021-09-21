3d3cb7f69b6a894cfb4899408df45c060973737d
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-09-21 13:51:09 +0300

    подкоректоровал класс

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -1,10 +1,10 @@
 export class CountdownTimer {
-    constructor({ selector, targetDate, onTick }) {
+    constructor({ selector, targetDate }) {
         this.targetDate = targetDate;
         this.selector = selector;
-        this.onTick = onTick;
         this.deltaTime;
         this.time;
+        this.start();
     }
 
     start() {
@@ -12,8 +12,29 @@ export class CountdownTimer {
             const currentTime = Date.now();
             this.deltaTime = this.targetDate - currentTime;
             this.time = this.getTimeComponents(this.deltaTime);
-            this.onTick(this.time);
+            this.clockInterface(this.time);
         }, 1000);
+    }
+
+    clockInterface({ days, hours, mins, secs }) {
+        let timerValue = document.querySelectorAll(`${this.selector} .value`);
+
+        timerValue.forEach(event => {
+            switch (event.dataset.value) {
+                case 'days':
+                    event.textContent = days;
+                    break;
+                case 'hours':
+                    event.textContent = hours;
+                    break;
+                case 'mins':
+                    event.textContent = mins;
+                    break;
+                case 'secs':
+                    event.textContent = secs;
+                    break;
+            }
+        });
     }
 
     getTimeComponents(time) {
--- src/js/timer.js
+++ src/js/timer.js
@@ -3,28 +3,6 @@ import { CountdownTimer } from './countdown-timer';
 const timer = new CountdownTimer({
     selector: '#timer-1',
     targetDate: new Date('Dec 31, 2021, 23:59:00'),
-    onTick: updateClockInterface,
 });
-
-function updateClockInterface({ days, hours, mins, secs }) {
-    let timerValue = document.querySelectorAll('#timer-1 .value');
-
-    timerValue.forEach(event => {
-        switch (event.dataset.value) {
-            case 'days':
-                event.textContent = days;
-                break;
-            case 'hours':
-                event.textContent = hours;
-                break;
-            case 'mins':
-                event.textContent = mins;
-                break;
-            case 'secs':
-                event.textContent = secs;
-                break;
-        }
-    });
-}
 
 timer.start();
